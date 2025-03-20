8699bd34f8c0193747c33b16bd6d24af5f9033e3
ellatomlinson <101608415+ellatomlinson@users.noreply.github.com>
2025-03-20 09:39:38 -0600

    Enable socket mode and add in slackbot

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,29 @@
-import express from "express";
+import pkg from '@slack/bolt';
+const { App } = pkg;
+import dotenv from "dotenv";
 
-const app = express();
-const port = process.env.PORT || 4343;
+// Load environment variables
+dotenv.config();
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+const SLACK_BOT_TOKEN = process.env.SLACK_BOT_TOKEN;
+const SLACK_SIGNING_SECRET = process.env.SLACK_SIGNING_SECRET;
+const APP_TOKEN = process.env.APP_TOKEN;
+
+if (!SLACK_BOT_TOKEN || !SLACK_SIGNING_SECRET) {
+  throw new Error("Missing Slack bot token or signing secret in environment variables");
+}
+
+const app = new App({
+  token: SLACK_BOT_TOKEN,
+  signingSecret: SLACK_SIGNING_SECRET,
+  socketMode: true,
+  appToken: APP_TOKEN, 
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+(async () => {
+  const port = process.env.PORT ? parseInt(process.env.PORT) : 3000;
+
+  // Start your app
+  await app.start(port);
+  console.log(`⚡️ Slack Bolt app is running on port ${port}!`);
+})();
