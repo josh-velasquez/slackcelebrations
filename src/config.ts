7779819eb39a9bd4596e176baf03ade0008c9936
Josh Velasquez <joshvelasquez@ip-192-168-1-84.us-west-2.compute.internal>
2025-03-20 20:29:58 -0600

    Add vercel config

--- src/config.ts
+++
@@ -1,9 +0,0 @@
-import dotenv from "dotenv";
-
-dotenv.config();
-
-export const config = {
-  supabaseUrl: process.env.SUPABASE_URL!,
-  supabaseKey: process.env.SUPABASE_KEY!,
-  port: process.env.PORT || 4343
-};
